b0e9107ce10ccf2ad4aefb49b0324aa294730d2f
江育瑋 <57368238+q2a3z@users.noreply.github.com>
2020-02-12 02:15:12 +0900

    Test

--- Assets/Script/Controller.cs
+++ Assets/Script/Controller.cs
@@ -4,22 +4,31 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
+    float posx;
     // Start is called before the first frame update
     void Start()
     {
-        
+       posx = GameObject.Find("WallL").transform.localPosition.z;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(posx);
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.position += transform.forward * 0.1f;
+            if(transform.localPosition.z < (posx - transform.localScale.z/2) )
+              transform.position += transform.forward * 0.1f;
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.position -= transform.forward * 0.1f;
+            if(transform.localPosition.z > -(posx - transform.localScale.z/2) )
+              transform.position -= transform.forward * 0.1f;
         }
+    }
+    //Detect collisions between the GameObjects with Colliders attached
+    void OnCollisionEnter(Collision collision)
+    {
+
     }
 }
